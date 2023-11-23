658f8979e2dbbb99073f309ff7d29f278b7d0608
dominikn <dominik.nwk@gmail.com>
2023-11-23 16:25:28 +0100

    trying to keep the distance

--- follow_apriltag/follow_apriltag/follow_apriltag.py
+++ follow_apriltag/follow_apriltag/follow_apriltag.py
@@ -21,6 +21,8 @@ class FollowAprilTagNode(Node):
 
     def update_robot_position(self):
         try:
+            # # Wait for up to 2 seconds for the transform to be available
+            # timeout = rclpy.duration.Duration(seconds=2)
             # Get the transform from the robot to the AprilTag
             trans = self.tf_buffer.lookup_transform('camera_link', 'tag36h11:4', rclpy.time.Time())
 
@@ -29,6 +31,11 @@ class FollowAprilTagNode(Node):
 
         except Exception as e:
             self.get_logger().info(f'Could not get transform: {e}')
+            # Create and publish Twist message
+            twist = Twist()
+            twist.linear.x = 0.0
+            twist.angular.z = 0.0
+            self.publisher.publish(twist)
 
     def process_transform_and_move_robot(self, transform):
         # Extract translation and rotation components
@@ -49,13 +56,24 @@ class FollowAprilTagNode(Node):
         self.get_logger().info(f"Distance to AprilTag: {distance} meters")
         self.get_logger().info(f"Angle to AprilTag: {angle} radians")
 
-        # Adjust velocities to maintain 50cm distance
+        # Adjust velocities to maintain 20cm distance
         linear_velocity = 0.0
         angular_velocity = 0.0
 
-        if distance > 0.5:  # If more than 50cm away
-            linear_velocity = min(0.5 * (distance - 0.5), 1.0)  # Speed capped at 1 m/s
-            angular_velocity = 3.0 * angle  # Adjust angular velocity based on angle
+        # Define a control constant for linear velocity (tune this based on your robot's characteristics)
+        linear_velocity_control_constant = 0.6
+        distance_to_keep = 0.25
+
+        # Check if the robot is too close or too far from the 20cm target
+        if distance > distance_to_keep:  # If more than 20cm away
+            # Move forward
+            linear_velocity = min(linear_velocity_control_constant * (distance - distance_to_keep), 1.0)  # Speed capped at 1 m/s
+        elif distance < distance_to_keep:  # If less than 20cm away
+            # Move backward
+            linear_velocity = -min(linear_velocity_control_constant * (distance_to_keep - distance), 1.0)  # Speed capped at 1 m/s
+
+        # Adjust angular velocity based on the angle to the AprilTag
+        angular_velocity = 3.0 * angle
 
         # Log the velocities being set
         self.get_logger().info(f"Setting linear velocity: {linear_velocity} m/s, angular velocity: {angular_velocity} rad/s")
